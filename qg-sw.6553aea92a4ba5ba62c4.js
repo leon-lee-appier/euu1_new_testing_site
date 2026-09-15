270e6a4e2634a87226d7b26fc293ecc6436b8b94
Leon Lee <leon.lee@appier.com>
2026-09-15 16:29:07 +0800

    fix

--- qg-sw.6553aea92a4ba5ba62c4.js
+++ qg-sw.6553aea92a4ba5ba62c4.js
@@ -1,2 +1,2 @@
-var appId = "a77283df271d4ddf548c";
+var appId = "6553aea92a4ba5ba62c4";
 importScripts("https://aiqua-stg-gcp-jp0-web-sdk.s3.ap-northeast-1.amazonaws.com/qgr-http/sdk/v3/r/qg-sw.js");
